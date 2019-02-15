cfd92b25dc4f63d88af3f7f4b39ddadf49b92f65
zhanghonghao <1404798723@qq.com>
2019-02-15 16:54:51 +0800

    发布子项目,子系统链接

---
+++ prd/src/main/java/com/anjubao/prd/PrdApplication.java
@@ -0,0 +1,14 @@
+package com.anjubao.prd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrdApplication.class, args);
+    }
+
+}
+
